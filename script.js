f4a7a6c820ff35dcda7f1b46f016e0a01b23ae4d
Alejandro Mendoza <89280136+AlejandroMendoza334@users.noreply.github.com>
2024-10-18 15:30:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector('.nav-responsive');
+const navMenu = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
