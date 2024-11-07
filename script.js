8f44b51ceae7d3cb65df379032c773ccf0fe7a2e
Alejandro Mendoza <89280136+AlejandroMendoza334@users.noreply.github.com>
2024-11-06 23:07:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ window.onload = function(){
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
 }
+
+function toggleMenu() {
+    document.querySelector('.contenedor-header nav ul').classList.toggle('show');
+}
+
+document.addEventListener('click', function (event) {
+    const navMenu = document.querySelector('.contenedor-header nav ul');
+    const navButton = document.querySelector('.nav-responsive');
+
+    if (navMenu.classList.contains('show') && !navMenu.contains(event.target) && !navButton.contains(event.target)) {
+        navMenu.classList.remove('show');
+    }
+});
+
+
